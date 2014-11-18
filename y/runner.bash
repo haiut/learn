c6976f6afa2d92adb4c007200f1755330d4b19dd
Alex Haiut <alex@blazemeter.com>
2014-11-18 18:50:07 +0200

    real test runs

--- y/runner.bash
+++ y/runner.bash
@@ -5,5 +5,17 @@
 export BZA=http://qa.a.blazemeter.com/api/latest
 export BZA_API_KEY="api_key=0b203f0b4cf4ee6abf99"
 
-curl -k ${BZA}/tests/5002315?${BZA_API_KEY}
-curl -k ${BZA}/tests/5002315/sessions?${BZA_API_KEY}
+#curl --silent --insecure ${BZA}/tests/5002315?${BZA_API_KEY}
+#curl --silent --insecure ${BZA}/tests/5002315/sessions?${BZA_API_KEY}
+
+SESSION_ID=$(curl --silent --insecure ${BZA}/tests/5002315/start?${BZA_API_KEY} | jq '.result.sessionsId[]' | tr -d \")
+echo "Test started .."
+
+TEST_RUN_STATUS=$(curl --silent --insecure ${BZA}/sessions/${SESSION_ID}?${BZA_API_KEY} | jq '.result.status' | tr -d \" )
+while [ "$TEST_RUN_STATUS" != "ENDED" ]; do
+        echo "Test status = " $TEST_RUN_STATUS
+        sleep 60
+        echo "Trying again .."
+	TEST_RUN_STATUS=$(curl --silent --insecure ${BZA}/sessions/${SESSION_ID}?${BZA_API_KEY} | jq '.result.status' | tr -d \" )
+done
+echo "Test status = " $TEST_RUN_STATUS
